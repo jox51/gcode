1f9ac5347c6869f23bb51d11ff0ac83915550261
Orlando L Dominici <orlandoldominici@Orlandos-iMac.local>
2024-04-22 06:39:42 -0400

    added user to approved list

--- app/Http/Middleware/CheckSubcriptionStatus.php
+++ app/Http/Middleware/CheckSubcriptionStatus.php
@@ -44,6 +44,6 @@ class CheckSubcriptionStatus {
      * @return array
      */
     protected function manualUserList(): array {
-        return ['test2@test.com', 'test@test.com', 'garythenumbersguy@gmail.com', 'test7@test.com', 'watercups09@yahoo.com'];
+        return ['test2@test.com', 'test@test.com', 'garythenumbersguy@gmail.com', 'test7@test.com', 'watercups09@yahoo.com', 'wolf_like_me_77@yahoo.com'];
     }
 }
